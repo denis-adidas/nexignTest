727e6ab729a42055858163f5157eb7bf601e0a75
Denis Tishin <veb-kamera2013@ya.ru>
2024-03-22 01:28:23 +0300

    complete

--- nexignTest/src/test/java/NexignStoreTest.java
+++ nexignTest/src/test/java/NexignStoreTest.java
@@ -1,6 +1,18 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.languagetool.JLanguageTool;
+import org.languagetool.*;
+import org.openqa.selenium.By;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
@@ -27,8 +39,9 @@ public class NexignStoreTest {
         open("https://nexign.com/ru");
         String pageContent = $("body").text();
         int nordCount = countWordOccurrences(pageContent, "Nord");
-        System.out.println("Количество упоминаний слова \"Nord\" на странице: " + nordCount);
+        System.out.println("Count of word: \"Nord\" on the page: " + nordCount);
     }
+
 
     private int countWordOccurrences(String text, String word) {
         int count = 0;
@@ -40,4 +53,46 @@ public class NexignStoreTest {
         }
         return count;
     }
+
+    @Test
+    public void testSpellingOnNexignPage() throws IOException {
+        openAndCheckSpellingOnFirstPages("https://nexign.com/ru", 5);
+    }
+
+    private void openAndCheckSpellingOnFirstPages(String url, int pageCount) throws IOException {
+        openMainPage(url);
+
+        List<String> spellingErrors = new ArrayList<>();
+
+        ElementsCollection links = getFirstPageLinks(pageCount);
+        for (int i = 0; i < links.size(); i++) {
+            String pageUrl = links.get(i).getAttribute("href");
+            String pageText = getPageText(pageUrl);
+            spellingErrors.addAll(checkSpelling(pageText));
+        }
+
+        System.out.println("Spelling errors on the first " + pageCount + " pages: " + spellingErrors);
+    }
+
+    private void openMainPage(String url) {
+        Selenide.open(url);
+    }
+
+    private ElementsCollection getFirstPageLinks(int count) {
+        return $$(By.cssSelector("a")).filter(visible).last(count);
+    }
+
+    private String getPageText(String url) {
+        Selenide.open(url);
+        return Selenide.$("body").getText();
+    }
+
+    private List<String> checkSpelling(String text) throws IOException {
+        JLanguageTool langTool = new JLanguageTool(Languages.getLanguageForShortCode("en"));
+        return langTool.check(text).stream()
+                .map(error -> error.getRule().getDescription())
+                .toList();
+    }
+
+
 }
